a3b6cf63b1b9825e9ebd41c9edc3c2aa5dffb911
tessier <t.tessier2002@gmail.com>
2023-07-31 22:22:25 +0200

    ajout de 2_saisie_utilisateur abordant string et getline

--- TUT_expert_youtube/1_Variables/1_Variables.cpp
+++ TUT_expert_youtube/1_Variables/1_Variables.cpp
@@ -4,18 +4,21 @@
 int main()
 {
     /*Préfices : 0b = binaire (0/1), 0 = octal (8), 0x = hexadecimal (16)*/
-
+    
     int a = 0b10011101;
     int b = 0236;
     int c = 0x15bf;
     int d = 14'256'365; /*permet de mieux lire le chiffre dans le codage*/
+    std::cout << "prefices \n" << std::endl;
     std::cout << "decimal de 10011101 (binaire) = " << a << std::endl;
     std::cout << "deciaml de 236 (octal) = " << b << std::endl;
     std::cout << "decimal de 15bf (hexadecimal) = " << c << std::endl;
     std::cout << d << std::endl;
 
+    std::cout << "\n" << std::endl;
+    
     /*Utilisation de string*/
-
+    std::cout << "utilisation string \n" << std::endl;
     std::string welcome{"Bien le bonsoir"};  /*on déclare une variable de type string (welcome) 
                                              à laquelle on affecte une valeur littérale (Bien le bonsoir)*/
     std::cout << "welcome valeur 1 = "<< welcome << std::endl;
