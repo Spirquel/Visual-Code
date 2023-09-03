36324f49846a5cd7e7c97192fd1f3f22cfb79d80
tessier <t.tessier2002@gmail.com>
2023-09-03 23:44:47 +0200

    ajout orientation objet + cours esiea

--- TUT_expert_youtube/0_main_empty/empty.cpp
+++ TUT_expert_youtube/0_main_empty/empty.cpp
@@ -3,9 +3,25 @@
 
 int main()
 {
-    auto a = "hello";
-    std::cout << a << std::endl;
+    char nom[50];
+    char prenom[50];
+
+    printf("Entrez votre nom : ");
+    scanf ("%s", nom);
+
+    printf("Entrez votre prenom : ");
+    scanf ("%s", prenom);
+
+printf("\n");
+
+    printf("voici votre nom : ");
+    printf("%s", nom);
+
+    printf(", ");
+
+    printf("voici votre prenom : ");
+    printf("%s", prenom);
+
 
     return 0;
-
 }
---
+++ TUT_expert_youtube/13_Classes_objet/13_Classes_objet.cpp
@@ -0,0 +1,11 @@
+#include "lew.hpp"
+#include <iostream>
+
+int main()
+{
+    lew p{};
+    lew p2{p};
+    lew p3{};
+
+    return 0;
+}
