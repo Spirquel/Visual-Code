ead80ba43606dcddff02a7da4ea87301f723e365
theo <t.tessier2002@gmail.com>
2023-10-12 18:14:29 +0200

    ajout commentaire au pense bete taille

--- Code_pense_bete/test_taille.c
+++ Code_pense_bete/test_taille.c
@@ -12,12 +12,12 @@ int main()
     typedef struct taille_impro IMpro;
 
 
-    printf("char : %d octets", sizeof(char));
-    printf("\nint : %d octets", sizeof(int));
-    printf("\nfloat : %d octets", sizeof(float));
-    printf("\nlong : %d octets", sizeof(long));
-    printf("\ndouble : %d octets", sizeof(double));
-    printf("\nImpro : %d octets", sizeof(IMpro));
+    printf("char : %d octets", sizeof(char)); /*1 octet*/
+    printf("\nint : %d octets", sizeof(int)); /*4 octets*/
+    printf("\nfloat : %d octets", sizeof(float)); /*4 octets*/
+    printf("\nlong : %d octets", sizeof(long)); /*4 octets en 32bits et 8 octets en 64 bits*/
+    printf("\ndouble : %d octets", sizeof(double)); /*8 octets*/
+    printf("\nImpro : %d octets", sizeof(IMpro)); /*16 octets (car float + int + double)*/
 
     return 0;
 }
